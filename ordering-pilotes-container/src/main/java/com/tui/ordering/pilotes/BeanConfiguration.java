f0e361755b7ff521c87df6969a7a3ebcebe7e01a
PONTIVEROS CALZADO Antonio Rafael <ar.pontiveros@soprasteria.com>
2023-11-23 15:32:58 +0100

    sep up project

---
+++ ordering-pilotes-container/src/main/java/com/tui/ordering/pilotes/BeanConfiguration.java
@@ -0,0 +1,9 @@
+package com.tui.ordering.pilotes;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+}
---
+++ ordering-pilotes-container/src/main/java/com/tui/ordering/pilotes/Main.java
@@ -0,0 +1,7 @@
+package com.tui.ordering.pilotes;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
